a083110a7eb893d98b7efe90b52b02122a3ce462
bmullan-pivotal <bmullan@pivotal.io>
2018-10-30 09:37:29 -0400

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+	res.send('hello world');
+});
+
+
+app.listen(8080,'0.0.0.0',() => {
+	console.log("hello world app is listening on port 8080");
+});
+
+
